ec11d3af8540462d3f18b843881db961c5170234
Sandeep <sandeep.rawat@jasperindia.com>
2013-08-08 18:41:40 +0530

    Added dummy implementation of interfaces

---
+++ User/UserService/src/main/java/com/sandy/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,12 @@
+package com.sandy.impl;
+
+import com.sandy.api.AuthenticationService;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+	public boolean authenticateUser(String userId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ User/UserService/src/main/java/com/sandy/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.sandy.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import com.sandy.api.UserService;
+import com.sandy.domain.User;
+
+public class UserServiceImpl implements UserService{
+
+	public void createUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public User getUser(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void activateUser(String userId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deactivateUser(String userId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<User> listUsersCreatedAfter(Date afterDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<User> listUsersCreatedBefore(Date beforeDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
