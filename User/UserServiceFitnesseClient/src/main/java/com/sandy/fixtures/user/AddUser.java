361a948f04efc3b8bc3e3f83d1cb9f0976671235
Sandeep <sandeep.rawat@jasperindia.com>
2013-08-08 19:40:25 +0530

    Added a fixture AddUser

---
+++ User/UserServiceFitnesseClient/src/main/java/com/sandy/fixtures/user/AddUser.java
@@ -0,0 +1,26 @@
+package com.sandy.fixtures.user;
+
+public class AddUser {
+	private String userId;
+	private String password;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void execute() {
+		
+	}
+}
