6192a517aec3f45439af55c531d31183e735f49c
Sandeep <sandeep.rawat@jasperindia.com>
2013-08-12 11:46:02 +0530

    Updated Service factory to return cached service instance

--- User/UserService/src/main/java/com/sandy/factory/ServiceFctory.java
+++ User/UserService/src/main/java/com/sandy/factory/ServiceFctory.java
@@ -5,8 +5,13 @@ import com.sandy.impl.UserServiceImpl;
 
 public class ServiceFctory {
 
+	private static UserServiceImpl userServiceImpl;
+
 	public static UserService getUserService() {
-		return new UserServiceImpl();
+		if (null == userServiceImpl) {
+			userServiceImpl = new UserServiceImpl();
+		}
+		return userServiceImpl;
 	}
 
 }
