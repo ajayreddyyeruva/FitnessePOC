dc10548950074b38c518cb41909484f125787e21
Sandeep <sandeep.rawat@jasperindia.com>
2013-08-12 13:41:28 +0530

    a logging statement

--- User/UserServiceFitnesseClient/src/main/java/com/sandy/fixtures/user/ListUsersCreatedAfterDate.java
+++ User/UserServiceFitnesseClient/src/main/java/com/sandy/fixtures/user/ListUsersCreatedAfterDate.java
@@ -20,6 +20,7 @@ public class ListUsersCreatedAfterDate {
 	
 	
 	public List<Object> query() {
+		System.out.println("Fetching users created after %s", startDate);
 		List<User> users = userService.listUsersCreatedAfter(new Date());
 		
 		QueryResultBuilder builder = new QueryResultBuilder(User.class);
