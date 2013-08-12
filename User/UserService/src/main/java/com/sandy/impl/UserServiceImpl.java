2cfc430d8311b3c63baa7b97146b7e00b17bb2ee
Sandeep <sandeep.rawat@jasperindia.com>
2013-08-12 13:39:28 +0530

    Updated service to list users created after a specific date

--- User/UserService/src/main/java/com/sandy/impl/UserServiceImpl.java
+++ User/UserService/src/main/java/com/sandy/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.sandy.impl;
 
 import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,8 +10,8 @@ import com.sandy.api.UserService;
 import com.sandy.domain.User;
 import com.sandy.exception.UserAlreadyExistsException;
 
-public class UserServiceImpl implements UserService{
-	
+public class UserServiceImpl implements UserService {
+
 	Map<String, User> usersMap = new LinkedHashMap<String, User>();
 
 	public User getUser(String userId) {
@@ -19,33 +20,38 @@ public class UserServiceImpl implements UserService{
 
 	public void activateUser(String userId) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void deactivateUser(String userId) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public List<User> listUsersCreatedAfter(Date afterDate) {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> usersCreatedAfterDate = new LinkedList<User>();
+		for (User user : usersMap.values()) {
+			if (user.getCreatedDate().after(afterDate))
+				usersCreatedAfterDate.add(user);
+		}
+		return usersCreatedAfterDate;
 	}
 
 	public List<User> listUsersCreatedBefore(Date beforeDate) {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> usersCreatedAfterDate = new LinkedList<User>();
+		return usersCreatedAfterDate;
 	}
 
 	public User createUser(String userId, String password) {
-		if(usersMap.containsKey(userId)) {
-			throw new UserAlreadyExistsException("User with userid already exists");
+		if (usersMap.containsKey(userId)) {
+			throw new UserAlreadyExistsException(
+					"User with userid already exists");
 		}
 		User user = new User(userId, password);
 		usersMap.put(userId, user);
 		return user;
 	}
-	
+
 	public boolean updateUser(String userId, String password, boolean active) {
 		boolean userUpdated = false;
 		User user = getUser(userId);
@@ -55,7 +61,7 @@ public class UserServiceImpl implements UserService{
 			userUpdated = true;
 		}
 		return userUpdated;
-		
+
 	}
 
 }
